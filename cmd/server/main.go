6d97551befd000ff9aae63a075905786266064d7
lsvishaal <lsvishaal7@gmail.com>
2026-03-11 17:19:00 +0000

    feat: implement HTTP server and conversion handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/lsvishaal/unit-converter/internal/converter"
+	"net/http"
+
+	httphandler "github.com/lsvishaal/unit-converter/internal/http"
 )
 
 func main(){
-	result := converter.ConvertLength(10, "kilometer", "mile")
-	fmt.Println(result)
+	// 1. on User visit to /convert URL, redirected to  ConvertHandler 
+	http.HandleFunc("/convert", httphandler.ConvertHandler)
+
+	//2. Start the infininte loop on port 8080
+	
+	var err error
+	fmt.Println("Server is starting on http://localhost:8080...")
+
+	//ListenAndServe locks program in infinite loop
+	//returns error only if server crashes fatally
+	err = http.ListenAndServe(":8080", nil)
+
+	// 3. If the loop breaks, catch the error
+	if err != nil {
+		fmt.Println("The server crashed:", err)
+	}
 }
--- internal/http/handler.go
+++ internal/http/handler.go
@@ -1,1 +1,46 @@
 package http
+
+import (
+	"fmt"
+	"net/http"
+	"strconv" // The Translator
+
+	"github.com/lsvishaal/unit-converter/internal/converter"
+)
+
+func ConvertHandler(w http.ResponseWriter, r *http.Request) {
+	
+	// STEP 1: Grab the text from the web address
+	// Example: ?value=10&from=kilometer&to=mile
+	var valueText string = r.URL.Query().Get("value")
+	var fromUnit string = r.URL.Query().Get("from")
+	var toUnit string = r.URL.Query().Get("to")
+
+	// STEP 2: Translate the text into a real number
+	var realNumber float64
+	var err error
+	
+	// Attempt the translation
+	realNumber, err = strconv.ParseFloat(valueText, 64)
+	
+	// If they typed "?value=apple", stop them
+	if err != nil {
+		fmt.Fprint(w, "Stop: Please type a real number.")
+		return 
+	}
+
+	// STEP 3: Do the math
+	var finalAnswer float64
+	var mathErr error
+	
+	finalAnswer, mathErr = converter.ConvertLength(realNumber, fromUnit, toUnit)
+
+	// If they typed a bad unit, stop them
+	if mathErr != nil {
+		fmt.Fprint(w, "Stop: ", mathErr)
+		return
+	}
+
+	// STEP 4: Put the answer on the silver tray (w) to send to the browser
+	fmt.Fprintf(w, "Success! Your answer is: %f", finalAnswer)
+}
